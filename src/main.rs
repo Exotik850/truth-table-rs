7887292d75fab73b55212099465952e622b93aa9
Exotik850 <kidkool850@gmail.com>
2024-09-14 23:07:22 -0500

    refactor main function to collect command line arguments as a string

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use truth_table_rs::Formula;
 
 fn main() {
     // let source = "((A & B) | C)";
-    let source = std::env::args().nth(1).unwrap_or("".to_string());
+    let source: String = std::env::args().skip(1).collect();
     let formula: Formula = (source.as_str()).into();
 
     // for node in formula.root.children() {
